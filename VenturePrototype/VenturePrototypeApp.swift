056544a00f88f1de543f7dfe2ea1c6695e175ae9
Philipp Sanktjohanser <venture@mein.gmx>
2022-12-26 06:50:51 +0100

    Initial Commit

---
+++ VenturePrototype/VenturePrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  VenturePrototypeApp.swift
+//  VenturePrototype
+//
+//  Created by Philipp Sanktjohanser on 26.12.22.
+//
+
+import SwiftUI
+
+@main
+struct VenturePrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
